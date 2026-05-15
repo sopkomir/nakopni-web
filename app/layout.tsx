f1bdf27077cca9bfa0f5c31313cd96e30fa9093f
unknown <sopkomir@gmail.com>
2026-05-15 17:06:52 +0200

    Change font on Anton

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import "./globals.css";
-
+import { Anton } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+const anton = Anton({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-anton",
+});
 
 export default function RootLayout({
   children,
@@ -10,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="sk">
-      <body>
+      <body className={anton.variable}>
 
       <div className="max-w-[1400px] mx-auto px-4 md:px-6 py-6">
 
