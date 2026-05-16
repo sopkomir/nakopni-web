308a35bf24fbe0b0feec4e22eb3a93b879d35073
unknown <sopkomir@gmail.com>
2026-05-16 19:19:11 +0200

    Fix Comments undefined crash

--- app/components/Comments.tsx
+++ app/components/Comments.tsx
@@ -1,29 +1,29 @@
 import CommentCard from "./CommentCard";
 
 type Props = {
-  articles: any[];
+  articles?: any[];
 };
 
-export default function Comments({ articles }: Props) {
+export default function Comments({ articles = [] }: Props) {
 
   return (
     <section className="space-y-10">
 
       {articles
-      .filter((article) => article.slug?.current)
-      .map((article) => (
+        .filter((article) => article.slug?.current)
+        .map((article) => (
 
-        <CommentCard
-          key={article._id}
-          slug={article.slug.current}
-          title={article.title}
-          description={article.excerpt}
-          image={article.image}
-          author={article.author}
-          date={new Date(article.publishedAt).toLocaleDateString("sk-SK")}
-        />
+          <CommentCard
+            key={article._id}
+            slug={article.slug.current}
+            title={article.title}
+            description={article.excerpt}
+            image={article.image}
+            author={article.author}
+            date={new Date(article.publishedAt).toLocaleDateString("sk-SK")}
+          />
 
-      ))}
+        ))}
 
     </section>
   );
