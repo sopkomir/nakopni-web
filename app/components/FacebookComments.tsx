b244dfb52b73d5dc19e53fa8036ae4c230af7c29
unknown <sopkomir@gmail.com>
2026-05-17 10:30:18 +0200

    Fix Facebook comments

--- app/components/FacebookComments.tsx
+++ app/components/FacebookComments.tsx
@@ -2,6 +2,10 @@
 
 import { useEffect } from "react";
 
+type Props = {
+  url: string;
+};
+
 declare global {
   interface Window {
     FB: any;
@@ -9,80 +13,55 @@ declare global {
   }
 }
 
-type Props = {
-  url: string;
-};
-
-export default function FacebookComments({
-  url,
-}: Props) {
+export default function FacebookComments({ url }: Props) {
 
   useEffect(() => {
-
-    const initFacebook = () => {
-
-      if (!window.FB) {
-        return;
-      }
-
-      window.FB.XFBML.parse();
-    };
-
-    if (window.FB) {
-
-      initFacebook();
-
-      return;
-    }
 
     window.fbAsyncInit = function () {
 
       window.FB.init({
-        appId:
-          process.env
-            .NEXT_PUBLIC_FACEBOOK_APP_ID,
-
+        appId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID,
         cookie: true,
-
         xfbml: true,
-
         version: "v19.0",
       });
 
-      initFacebook();
+      window.FB.XFBML.parse();
     };
 
-    if (
-      !document.getElementById(
-        "facebook-jssdk"
-      )
-    ) {
+    const existingScript =
+      document.getElementById("facebook-jssdk");
 
-      const script =
-        document.createElement("script");
+    if (!existingScript) {
+
+      const script = document.createElement("script");
 
       script.id = "facebook-jssdk";
+      script.async = true;
+      script.defer = true;
+      script.crossOrigin = "anonymous";
 
       script.src =
         "https://connect.facebook.net/en_US/sdk.js";
 
-      script.async = true;
-
-      script.defer = true;
-
       document.body.appendChild(script);
+
+    } else {
+
+      if (window.FB) {
+        window.FB.XFBML.parse();
+      }
+
     }
 
-  }, []);
+  }, [url]);
 
   return (
-
     <div
       className="fb-comments"
       data-href={url}
       data-width="100%"
       data-numposts="5"
     />
-
   );
 }
