081b3a9d812db1318a44188834cd01a36df86aaf
unknown <sopkomir@gmail.com>
2026-05-17 10:54:48 +0200

    Add privacy page

---
+++ app/privacy/page.tsx
@@ -0,0 +1,15 @@
+export default function PrivacyPage() {
+  return (
+    <main className="max-w-3xl mx-auto py-20 px-6">
+      <h1 className="text-5xl font-black mb-8">
+        Ochrana osobných údajov
+      </h1>
+
+      <p className="text-lg leading-8">
+        Nakopni.sk neuchováva žiadne osobné údaje
+        používateľov mimo údajov poskytovaných
+        platformou Facebook v rámci komentárov.
+      </p>
+    </main>
+  );
+}
