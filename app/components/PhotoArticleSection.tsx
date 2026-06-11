eca3e57be08122fe6b6ce55016f5d05235c60a4f
unknown <sopkomir@gmail.com>
2026-06-11 18:37:04 +0200

    Repair humor11

--- app/components/PhotoArticleSection.tsx
+++ app/components/PhotoArticleSection.tsx
@@ -41,7 +41,7 @@ export default function PhotoArticleSection({ posts }: Props) {
           <h2
             className="
               flex
-              ml-auto
+              items-center
               gap-4
               pl-8
               text-4xl
