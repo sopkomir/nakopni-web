e5d0b3f416fbb4accd6bb5ee9b6e5cba70c9fe75
unknown <sopkomir@gmail.com>
2026-07-14 20:40:42 +0200

    Zdielanie fotoclankov4

--- app/fotoclanok/[slug]/opengraph-image.tsx
+++ app/fotoclanok/[slug]/opengraph-image.tsx
@@ -1,4 +1,9 @@
 import { ImageResponse } from "next/og";
+import { groq } from "next-sanity";
+import { client } from "@/app/lib/sanity";
+import { urlForImage } from "@/app/lib/image";
+
+export const runtime = "nodejs";
 
 export const size = {
   width: 1200,
@@ -7,23 +12,128 @@ export const size = {
 
 export const contentType = "image/png";
 
-export default async function Image() {
+export default async function Image({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await params;
+
+  const post = await client.fetch(
+    groq`
+      *[
+        _type == "fotoclanok" &&
+        slug.current == $slug
+      ][0]{
+        title,
+        image
+      }
+    `,
+    { slug }
+  );
+
+  const imageUrl = post?.image
+    ? urlForImage(post.image)
+        .width(1200)
+        .height(630)
+        .fit("crop")
+        .url()
+    : null;
+
   return new ImageResponse(
     (
       <div
         style={{
           width: "100%",
           height: "100%",
-          background: "#111",
+          position: "relative",
           display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          fontSize: 64,
-          fontWeight: 700,
+          overflow: "hidden",
+          background: "#111",
         }}
       >
-        Nakopni.sk
+        {imageUrl && (
+          <img
+            src={imageUrl}
+            alt=""
+            style={{
+              position: "absolute",
+              inset: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        )}
+
+        {/* Gradient */}
+        <div
+          style={{
+            position: "absolute",
+            inset: 0,
+            background:
+              "linear-gradient(to top, rgba(0,0,0,.88), rgba(0,0,0,.15))",
+          }}
+        />
+
+        {/* Logo */}
+        <img
+          src="https://www.nakopni.sk/logo-og.png"
+          alt=""
+          style={{
+            position: "absolute",
+            top: 40,
+            left: 40,
+            width: 260,
+          }}
+        />
+
+        {/* Označenie */}
+        <div
+          style={{
+            position: "absolute",
+            top: 42,
+            right: 40,
+            background: "#000",
+            color: "#fff",
+            padding: "12px 24px",
+            borderRadius: 999,
+            fontSize: 28,
+            fontWeight: 700,
+          }}
+        >
+          FOTOČLÁNOK
+        </div>
+
+        {/* Titulok */}
+        <div
+          style={{
+            position: "absolute",
+            left: 50,
+            bottom: 95,
+            right: 50,
+            color: "white",
+            fontSize: 58,
+            fontWeight: 800,
+            lineHeight: 1.1,
+          }}
+        >
+          {post.title}
+        </div>
+
+        {/* slogan */}
+        <div
+          style={{
+            position: "absolute",
+            left: 50,
+            bottom: 40,
+            color: "#ff7a00",
+            fontSize: 28,
+            fontWeight: 600,
+          }}
+        >
+          Hľadáme riešenia ako nakopnúť Slovensko
+        </div>
       </div>
     ),
     size
